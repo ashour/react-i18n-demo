3ac8d1229404879aff03abf332dcb402527019b7
Mohammad Ashour <ashour.mohammad@gmail.com>
2018-02-17 12:58:23 +0000

    Added links to license

--- front/src/components/AppFooter.js
+++ front/src/components/AppFooter.js
@@ -10,7 +10,7 @@ export default () => (
                   dangerouslySetInnerHTML={{__html: t('footer_copy', {
                     author_url: 'https://ashour.ca',
                     react_url: 'https://reactjs.org/',
-                    license_url: '#',
+                    license_url: 'https://github.com/ashour/react-i18n-demo/blob/master/LICENSE',
                     article_url: '#'
             })}} />
         </Nav>
